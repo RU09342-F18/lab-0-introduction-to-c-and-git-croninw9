377aa7f99a8eef45ec486f7eded1b8a64ee32c36
croninw9 <36242628+croninw9@users.noreply.github.com>
2018-09-10 11:22:48 -0400

    Add files via upload

---
+++ Assignment/math.c
@@ -0,0 +1,51 @@
+/*
+    Name: math.c
+    Author: William Cronin
+    Description: Apply a math operation to num1 and num2
+
+    List of Operations:
+    + Add (num1 + num2)
+    - Subtract (num1 - num2)
+    * Multiply (num1 * num2)
+    / Divide (num1 / num2)
+    % Modulus (num1 % num2)
+    < Left Shift (num1 << num2)
+    > Right Shift (num1 >> num2)
+    & Bitwise AND (num1 & num2)
+    | Bitwise OR (num1 | num2)
+    ^ Bitwise XOR (num1 ^ num2)
+    ~ Bitwise Inverse (~num1)
+
+
+*/
+
+int math(int num1, int num2, char Operator)
+{
+    switch(Operator)
+    case'+':
+        return num1 + num2;
+    case'-':
+        return num1 - num2;
+    case'*':
+        return num1 * num2;
+    case'/':
+        return num1 / num2;
+    case'%':
+        return num1 % num2;
+    case'<':
+        return num1 << num2;
+    case'>':
+        return num1 >> num2;
+    case'&':
+        return num1 & num2;
+    case'|':
+        return num1 | num2;
+    case'^':
+        return num1 ^ num2;
+    case'~':
+        return ~num1;
+    default:
+        printf("Please enter a valid operation");
+        return 0;
+
+}
